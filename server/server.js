25bef1d119733aed02072291a3d0ff26265d4a34
Timothy <02.timoha.0@gmail.com>
2021-06-21 11:18:19 +0300

    1

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();//обрабатываем поступающие запр
 const cart = require('./cartRouter');
 
 app.use(express.json());
-app.use('/', express.static('public'));
+app.use('/', express.static('./'));
 app.use('/api/cart', cart);
 
 
@@ -15,8 +15,8 @@ app.use('/api/cart', cart);
 
 app.get('/api/products', (req, res) => {
     fs.readFile('server/db/products.json', 'utf-8', (err, data) => {
-        if(err){
-            res.sendStatus(404, JSON.stringify({result:0, text: err}));
+        if (err) {
+            res.sendStatus(404, JSON.stringify({ result: 0, text: err }));
         } else {
             res.send(data);
         }
